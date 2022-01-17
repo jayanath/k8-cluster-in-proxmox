c1fc4c4ee1b7cad036c84d9bb78d9f7b0ed9ed08
Jayanath Amaranayake <jay@flood.io>
2022-01-17 20:26:06 +1100

    known hosts cleanup update

--- ssh-cleanup.sh
+++ ssh-cleanup.sh
@@ -8,8 +8,6 @@ ssh-keygen -R master.example.com
 ssh-keygen -R worker0.example.com
 ssh-keygen -R worker1.example.com
 ssh-keygen -R master
-ssh-keygen -R worker0
-ssh-keygen -R worker1
 ssh-keygen -R worker-0
 ssh-keygen -R worker-1
 
